ae92b8445522fc0357756bf97e67186060445f97
Kevin Salazar <kast46.15@gmail.com>
2020-12-31 13:33:35 -0500

    Add files via upload

--- ubuntu/a.sh
+++ ubuntu/a.sh
@@ -1,5 +1,3 @@
 #/bin/bash -c "sleep 2 && xrandr --output eDP --brightness 0.65"
 #/bin/bash -c "sleep 2 && /home/kevin/./a.sh"
-xrandr --output eDP --brightness 0.7
-
-
+xrandr --output eDP --brightness 0.75
